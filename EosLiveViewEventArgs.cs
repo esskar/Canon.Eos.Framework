3f7e2e0db99383aeb186e428fb80f595046d8e8c
esskar <sk@intertivity.com>
2012-01-19 21:43:52 +0100

    Add missing file.

---
+++ EosLiveViewEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+namespace Canon.Eos.Framework
+{
+    [Obsolete("Use EosLiveViewEventArgs instead.")]
+    public class LiveViewEventArgs : EventArgs 
+    { 
+        internal LiveViewEventArgs(Image image)
+        {
+            this.Image = image;
+        }
+
+        public Image Image { get; private set; }
+    }
+
+    public class EosLiveViewEventArgs : LiveViewEventArgs
+    {
+        internal EosLiveViewEventArgs(Image image)
+            : base(image) { }
+    }    
+}
